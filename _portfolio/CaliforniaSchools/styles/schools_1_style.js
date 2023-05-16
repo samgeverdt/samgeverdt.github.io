156a38a6873b22ece25d1db6120060b33c589010
Sam Geverdt <124743071+samgeverdt@users.noreply.github.com>
2023-05-16 18:55:20 -0400

    added californiaschools

---
+++ _portfolio/CaliforniaSchools/styles/schools_1_style.js
@@ -0,0 +1,32 @@
+var size = 0;
+var placement = 'point';
+
+var style_schools_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = ""
+    var labelText = "";
+    size = 0;
+    var labelFont = "13.0px \'Arial\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if (feature.get("name") !== null && resolution > 0 && resolution < 280) {
+        labelText = String(feature.get("name"));
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 8.0 + size, points: 5,
+            radius2: 4.0, stroke: new ol.style.Stroke({color: 'rgba(50,87,128,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1}), fill: new ol.style.Fill({color: 'rgba(72,123,182,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
